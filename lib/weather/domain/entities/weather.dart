098b467dc5ce49925494a876f6b46684ac64fd16
Abdullah Mohmaed <abdullahata000@gmail.com>
2026-09-23 17:54:07 +0300

    build base of project

---
+++ lib/weather/domain/entities/weather.dart
@@ -0,0 +1,15 @@
+class Weather {
+  final int id;
+  final int pressure;
+  final String cityName;
+  final String main;
+  final String description;
+
+  Weather(
+      this.id,
+      this.pressure,
+      this.cityName,
+      this.main,
+      this.description,
+      );
+}
