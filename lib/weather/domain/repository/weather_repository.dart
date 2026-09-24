fe222804d864f4aa4759a4a1ca9d1d3f9d20ee72
Abdullah Mohmaed <abdullahata000@gmail.com>
2026-09-24 17:57:00 +0300

    build base of project

--- lib/weather/domain/repository/weather_repository.dart
+++ lib/weather/domain/repository/weather_repository.dart
@@ -1,1 +1,5 @@
-abstract class WeatherRepository {}
+import 'package:weather/weather/domain/entities/weather.dart';
+
+abstract class WeatherRepository {
+  Future<Weather> getWeatherByCityName(String cityName);
+}
