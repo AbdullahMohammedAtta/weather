cc3d6ae399c998766530f99ca88d7cc101c472ac
Abdullah Mohmaed <abdullahata000@gmail.com>
2026-09-26 19:58:07 +0300

    data source

--- lib/core/utils/constants.dart
+++ lib/core/utils/constants.dart
@@ -1,4 +1,5 @@
 class AppConstants {
   static const baseurl = "http://api.openweathermap.org/data/2.5";
+  static const appId = "d1c6ce77ed24b4edfa619dfac796d64f";
 
 }
---
+++ lib/weather/data/datasource/remote_datasource.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:weather/core/utils/constants.dart';
+import 'package:weather/weather/data/models/weather_model.dart';
+import 'package:dio/dio.dart';
+
+abstract class BaseRemoteDataSource{
+
+  Future<WeatherModel?> getWeatherByCuntryName(String countryName);
+
+}
+
+
+class RemoteDatasource implements BaseRemoteDataSource{
+  @override
+  Future<WeatherModel?> getWeatherByCuntryName(countryName)async {
+      final dio = Dio();
+      var response = await dio.get('${AppConstants.baseurl}/weather?q=$countryName&appid=${AppConstants.appId}');
+      print(response);
+      return WeatherModel.fromJson(jsonDecode(response.data));
+
+
+  }
+
+
+
+}
